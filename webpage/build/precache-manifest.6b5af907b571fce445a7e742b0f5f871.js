b514b9cbfdea09e2ea3a40c694ed007402bdc57c
Arjun Ramesh <arjunramesh@utexas.edu>
2019-12-23 10:33:34 +0530

    Restructured

---
+++ webpage/build/precache-manifest.6b5af907b571fce445a7e742b0f5f871.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcaa5b42370afbb0f88f4316c3395561",
+    "url": "/Personal-Website/index.html"
+  },
+  {
+    "revision": "49639d2196d08571a865",
+    "url": "/Personal-Website/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a04da2569faed5103ec3",
+    "url": "/Personal-Website/static/js/2.70d64ca0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Personal-Website/static/js/2.70d64ca0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "49639d2196d08571a865",
+    "url": "/Personal-Website/static/js/main.5960aabb.chunk.js"
+  },
+  {
+    "revision": "05584a8f149d6e3b055e",
+    "url": "/Personal-Website/static/js/runtime-main.369788dc.js"
+  },
+  {
+    "revision": "47ed275de9980ea2af119beadf64ec66",
+    "url": "/Personal-Website/static/media/Resume.47ed275d.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Personal-Website/static/media/logo.5d5d9eef.svg"
+  }
+]);
